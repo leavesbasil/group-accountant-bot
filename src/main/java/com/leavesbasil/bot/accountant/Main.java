58d9040bfbb7ced45854f30659c3295132786b11
vasily <shchrbkvvs@gmail.com>
2018-12-28 17:13:47 +0300

    Use spring-boot

--- src/main/java/com/leavesbasil/bot/accountant/Main.java
+++ src/main/java/com/leavesbasil/bot/accountant/Main.java
@@ -1,37 +1,14 @@
 package com.leavesbasil.bot.accountant;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.io.IOException;
-import java.util.Properties;
-
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("========== Run telegram bot application ==========");
         ApiContextInitializer.init();
-        Properties properties = new Properties();
-        try {
-            properties.load(Main.class.getResourceAsStream("/application.properties"));
-        } catch (IOException e) {
-            System.out.println("Read parameters failed!");
-            e.printStackTrace();
-            throw new IllegalArgumentException(e);
-        }
-        String name = properties.getProperty("telegram.bot.name");
-        String token = properties.getProperty("telegram.bot.token");
-
-        System.out.println(name + " register..");
-        TelegramBotsApi botsApi = new TelegramBotsApi();
-        try {
-
-            botsApi.registerBot(new GroupAccountantBot(name, token));
-        } catch (TelegramApiException e) {
-            System.out.println(name + " register failed!");
-            e.printStackTrace();
-            throw new IllegalStateException(e);
-        }
-        System.out.println(name + " registered!");
+        SpringApplication.run(Main.class, args);
     }
 }
