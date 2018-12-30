e6d92d328163ed12a22c25c3b11c0cd807ff4a9a
vasily <shchrbkvvs@gmail.com>
2018-12-30 20:48:15 +0300

    Comment telegram

--- src/main/java/com/leavesbasil/bot/accountant/Main.java
+++ src/main/java/com/leavesbasil/bot/accountant/Main.java
@@ -2,19 +2,18 @@ package com.leavesbasil.bot.accountant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("========== Run telegram bot application ==========");
-        try {
-            ApiContextInitializer.init();
-        } catch (Exception e) {
-            System.out.println("========== Telegram api initialize error! ==========");
-            throw e;
-        }
-        System.out.println("========== Telegram api initialize success! ==========");
+//        System.out.println("========== Run telegram bot application ==========");
+//        try {
+//            ApiContextInitializer.init();
+//        } catch (Exception e) {
+//            System.out.println("========== Telegram api initialize error! ==========");
+//            throw e;
+//        }
+//        System.out.println("========== Telegram api initialize success! ==========");
         SpringApplication.run(Main.class, args);
     }
 }
