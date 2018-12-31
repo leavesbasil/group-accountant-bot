889dfadc272f542464ae3c331318ef7cc6280f3f
vasily <shchrbkvvs@gmail.com>
2018-12-31 17:06:02 +0300

    Bot enable

--- src/main/java/com/leavesbasil/bot/accountant/GroupAccountantBot.java
+++ src/main/java/com/leavesbasil/bot/accountant/GroupAccountantBot.java
@@ -1,12 +1,13 @@
 package com.leavesbasil.bot.accountant;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-//@Component
+@Component
 public class GroupAccountantBot extends TelegramLongPollingBot {
 
     @Value("${spring.telegram.bot.name}")
--- src/main/java/com/leavesbasil/bot/accountant/Main.java
+++ src/main/java/com/leavesbasil/bot/accountant/Main.java
@@ -2,18 +2,19 @@ package com.leavesbasil.bot.accountant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("========== Run telegram bot application ==========");
-//        try {
-//            ApiContextInitializer.init();
-//        } catch (Exception e) {
-//            System.out.println("========== Telegram api initialize error! ==========");
-//            throw e;
-//        }
-//        System.out.println("========== Telegram api initialize success! ==========");
+        System.out.println("========== Run telegram bot application ==========");
+        try {
+            ApiContextInitializer.init();
+        } catch (Exception e) {
+            System.out.println("========== Telegram api initialize error! ==========");
+            throw e;
+        }
+        System.out.println("========== Telegram api initialize success! ==========");
         SpringApplication.run(Main.class, args);
     }
 }
