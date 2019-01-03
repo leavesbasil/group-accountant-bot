0067f9e7f072167a0cd8c1afd521bb1b717fc5d8
Vasiliy <Shchrbkvvs@gmail.com>
2019-01-04 01:49:14 +0300

    Add draft commands

---
+++ src/main/java/com/leavesbasil/bot/accountant/command/Help.java
@@ -0,0 +1,9 @@
+package com.leavesbasil.bot.accountant.command;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.helpCommand.HelpCommand;
+
+@Component
+public class Help extends HelpCommand {
+    //nothing
+}
---
+++ src/main/java/com/leavesbasil/bot/accountant/command/Join.java
@@ -0,0 +1,43 @@
+package com.leavesbasil.bot.accountant.command;
+
+import com.leavesbasil.bot.accountant.service.Accountant;
+import com.leavesbasil.bot.accountant.service.AccountantManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.logging.BotLogger;
+
+@Component
+public class Join extends BotCommand {
+
+    @Autowired
+    private AccountantManager accountantManager;
+
+    /**
+     * Construct a command
+     */
+    public Join() {
+        super("join", "join to accounting group");
+    }
+
+    @Override
+    public void execute(AbsSender absSender, User user, Chat chat, String[] arguments) {
+        Accountant accountant = accountantManager.getAccountant(chat);
+        accountant.registerUser(user);
+
+        String answer = String.format("%s joined to accounting group!", user.getUserName());
+        SendMessage message = new SendMessage()
+                .setChatId(chat.getId())
+                .setText(answer);
+        try {
+            absSender.execute(message);
+        } catch (TelegramApiException e) {
+            BotLogger.error("JOIN_COMMAND", e);
+        }
+    }
+}
