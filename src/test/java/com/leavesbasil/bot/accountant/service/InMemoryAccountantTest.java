ca7c939992533482a22c68cbacb88120d20c1d83
Vasiliy <Shchrbkvvs@gmail.com>
2019-01-06 00:03:18 +0300

    Calculate command draft

--- src/test/java/com/leavesbasil/bot/accountant/service/InMemoryAccountantTest.java
+++ src/test/java/com/leavesbasil/bot/accountant/service/InMemoryAccountantTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.telegram.telegrambots.meta.api.objects.User;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
 
 import static org.junit.Assert.assertEquals;
 
@@ -24,5 +27,34 @@ public class InMemoryAccountantTest {
         sut.registerUser(user);
         sut.addPurchase(user, "Tomato", BigDecimal.valueOf(25.5));
         assertEquals(1, sut.getPurchases().size());
+    }
+
+    @Test
+    public void testCalculate() {
+        User user1 = new User(1, "Garry", false, "Poter", "Oculus", "RU");
+        User user2 = new User(2, "Germiona", false, "Grandger", "Botan", "RU");
+        User user3 = new User(3, "Ron", false, "Weasly", "Red", "RU");
+        Accountant sut = new InMemoryAccountant();
+        sut.registerUser(user1);
+        sut.registerUser(user2);
+        sut.registerUser(user3);
+        sut.addPurchase(user1, "Tomato", BigDecimal.valueOf(100));
+        sut.addPurchase(user2, "Cucumber", BigDecimal.valueOf(0));
+        sut.addPurchase(user3, "Potato", BigDecimal.valueOf(0));
+        Set<Payment> payments = sut.calculate();
+        payments.forEach(payment -> System.out.println(
+                payment.getSource().getUserName()
+                        .concat("-[")
+                        .concat(String.valueOf(payment.getValue()))
+                        .concat("]->")
+                        .concat(payment.getTarget().getUserName()))
+        );
+        assertEquals(2, payments.size());
+    }
+
+    @Test
+    public void name() {
+        TreeSet<Integer> treeSet = new TreeSet<>(Arrays.asList(10, 2, 10, 1, 1, 10, -2));
+        treeSet.forEach(System.out::println);
     }
 }
